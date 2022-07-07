94c271fed7707fcb1c6d0f7f1da2ca51746665b3
Jacob <47867208+CodingRhino93@users.noreply.github.com>
2022-07-07 16:12:24 +0100

    Almost done

--- src/main/java/com/example/demo/NodeRepositoryImpl.java
+++ src/main/java/com/example/demo/NodeRepositoryImpl.java
@@ -1,33 +1,54 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.Collection;
 
+@Repository
 public class NodeRepositoryImpl implements NodeRepository
 {
 
+    @PersistenceContext
+    protected EntityManager entityManager;
 
     @Override
     public Collection<Node> getNodes() {
-        return null;
+        String jpql = "select n from Node n";
+        TypedQuery<Node> query = entityManager.createQuery(jpql, Node.class);
+        return query.getResultList();
     }
 
     @Override
     public Node getNode(int id) {
-        return null;
+        return entityManager.find(Node.class, id);
     }
 
     @Override
+    @Transactional
     public int addNewNode(Node node) {
-        return 0;
+        entityManager.persist(node);
+        entityManager.flush();
+        return node.getNodeID();
     }
 
     @Override
+    @Transactional
     public boolean updateNode(int id, String name) {
-        return false;
+        Node node = entityManager.find(Node.class, id);
+        if (node == null) {
+            return false;
+        }
+        else {
+            node.setName(name);
+            return true;
+        }
     }
-
     @Override
     public void deleteNode() {
-
+        
     }
 }
--- src/main/java/com/example/demo/NodeRestController.java
+++ src/main/java/com/example/demo/NodeRestController.java
@@ -1,8 +1,54 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.util.Collection;
+
+@Controller
+@CrossOrigin
 public class NodeRestController
 {
+    @Autowired
+    private NodeRepository repository;
 
+    @GetMapping(produces={"application/json","application/xml"})
+    public ResponseEntity<Collection<Node>> getAllNodes() {
+        Collection<Node> result = repository.getNodes();
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PostMapping(consumes={"application/json","application/xml"},
+            produces={"application/json","application/xml"})
+    public ResponseEntity<Node> insertNode(@RequestBody Node node) {
+        int id = repository.addNewNode(node);
+        node.setNodeID(id);
+        URI uri = URI.create("/" + id);
+        return ResponseEntity.created(uri).body(node);
+    }
+
+    @GetMapping(value="/{id}", produces={"application/json","application/xml"})
+    public ResponseEntity<Node> getNode(@PathVariable int id) {
+        Node result = repository.getNode(id);
+        if (result == null) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            return ResponseEntity.ok().body(result);
+        }
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity modifyName(@PathVariable int id, @RequestParam String name) {
+        if (!repository.updateNode(id, name)) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            return ResponseEntity.ok().build();
+        }
+    }
 
 }
