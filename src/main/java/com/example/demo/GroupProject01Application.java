d9f0912b4709e0f0c2fcfa883ffc45119a802b0d
sleahy92 <68637775+sleahy92@users.noreply.github.com>
2022-07-07 13:28:54 +0100

    Git Test
    Simple Comment GitTest

--- src/main/java/com/example/demo/GroupProject01Application.java
+++ src/main/java/com/example/demo/GroupProject01Application.java
@@ -1,10 +1,15 @@
+/*Spring boot project - Jakub & Shauna
+* Date: 07-07-2022
+* The purpose of this project is to create a microservice - simple network management system.  */
+
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroupProject01Application {
+public class GroupProject01Application
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupProject01Application.class, args);
